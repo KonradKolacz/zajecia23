50c3899c0c80e606a3d1e7fbb877fd0bbda6d5b9
KonradKolacz <konrad8540@wp.pl>
2023-01-07 09:06:43 +0100

    TASK-55 remove comments and add git description

--- src/main/java/com/example/zajecia23/service/CarService.java
+++ src/main/java/com/example/zajecia23/service/CarService.java
@@ -7,11 +7,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class CarService {
+
     private final CarRepository carRepository;
 
     public Car addCar(Car car){
